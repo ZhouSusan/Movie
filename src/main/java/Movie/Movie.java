594e4255193edcfd6f419d98b20c3208ae6087c4
ZhouSusan <szhou089@gmail.com>
2022-11-28 11:08:43 -0800

    updated attributes and constructor in Movie class - Movie.java

--- src/main/java/Movie/Movie.java
+++ src/main/java/Movie/Movie.java
@@ -9,19 +9,21 @@ public class Movie {
     private String title;
     private String genre;
     private String director;
-    private String releaseDate;
+    private int productionYear;
     private double rating;
     private int runTime;
+    private String ageRestrictions;
     private List<String> actors;
 
-    public Movie(int movieId, String title, String genre, String director, String releaseDate, int rating, int runTime) {
+    public Movie(int movieId, String title, String genre, String director, int productionYear, int rating, int runTime, String ageRestrictions) {
         this.movieId = movieId;
         this.title = title;
         this.genre = genre;
         this.director = director;
-        this.releaseDate = releaseDate;
+        this.productionYear = productionYear;
         this.rating = rating;
         this.runTime = runTime;
+        this.ageRestrictions = ageRestrictions;
 
         actors = new ArrayList<String>();
     }
